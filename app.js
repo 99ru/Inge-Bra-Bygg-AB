35687787377c1a6390a99e111eca43eb9da80c8b
99ru <yan.99ru@gmail.com>
2022-05-01 20:38:08 +0200

    push before i do something stoopid

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const connection = require("./database/connection");
+const connection = require("./database/connection"); 
+const routes = require("./routes");
 
 require("dotenv").config();
 
 // middlewares
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(fileUpload()); 
-app.use(express.json()); 
+/* app.use(fileUpload());  */ // allows us to upload files
+app.use(express.json()); // allows us to use req.body
 
 // Routes
 
