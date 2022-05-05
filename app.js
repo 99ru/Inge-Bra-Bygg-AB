dad3b8e0f8a858c36f1976dc855e670bbdd6200f
Yan <89448433+99ru@users.noreply.github.com>
2022-05-05 10:32:10 +0200

    fixed so that only admin can see all users

--- app.js
+++ app.js
@@ -10,16 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 /* app.use(fileUpload()); for later */ 
 
-
 // Routes
 app.use('/', routes.auth);                     
 app.use('/users', routes.users); 
 app.use('/tasks', routes.tasks); 
 app.use('/tasks/:id/msg', routes.tasks); 
 app.use('/tasks/:id/img', routes.tasks); 
-
-
-
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,21 +2,24 @@ const Users = require("../models/User");
 const Tasks = require("../models/Task");
 
 module.exports = {
-
   allUsers: async (req, res) => {
-    const users = await Users.findAll({ attributes: { exclude: ["password"] } });
-    res.json({ users });
+    if (req.user.role === "admin") {
+      const users = await Users.findAll({
+        attributes: { exclude: ["password"] },
+      });
+      res.json({ users });
+    } else {
+      throw new Error("You dont have permission to view all users");
+    }
   },
 
   // Admin ska kunna skapa nya konton och kunna radera resurser
-  createUser: async (req, res) => { 
+  createUser: async (req, res) => {
     if (req.user.role === "admin") {
       const user = await Users.create(req.body);
       res.json({ user });
     } else {
       throw new Error("You are not allowed to create new users");
     }
-  }
+  },
 };
-
-
