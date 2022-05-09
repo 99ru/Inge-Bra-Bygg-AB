095f61ce16caffd7bc11f79f0fc5656e219df4a4
Yan <89448433+99ru@users.noreply.github.com>
2022-05-09 09:12:02 +0200

    pushing latest

--- controllers/ImgController.js
+++ controllers/ImgController.js
@@ -1,24 +1,21 @@
-const fs = require('fs') // allows us to read and write files
-const path = require('path') // allows us to get the path to a file
+const fs = require("fs"); // allows us to read and write files
+const path = require("path"); // allows us to get the path to a file
 
 module.exports = {
-
-  getAll: async (req, res) => { 
-    const images = fs.readdirSync(path.join('public', 'images')
-    );
-    res.json({images})
+  getAll: async (req, res) => {
+    const images = fs.readdirSync(path.join("public", "images"));
+    res.json({ images });
   },
 
   upload: (req, res) => {
-    console.log(req.files)
-    if (!req.files.image.mimetype.startsWith('image/')) {
+    console.log(req.files);
+    if (!req.files.image.mimetype.startsWith("image/")) {
       console.log("Not an image");
-      /* throw new InvalidFile("Invaild file, must be an image"); */
     }
 
     if (fs.existsSync(path.join("public", "images", req.files.image.name))) {
       console.log("File already exists");
-      /* throw new Error("error") */ (req.files.image.name);
+      req.files.image.name;
     }
 
     fs.copyFileSync(
@@ -27,4 +24,4 @@ module.exports = {
     );
     res.json({ message: "Image uploaded" });
   },
-}
+};
